0182157b9393f274519fcc22328c376d78c1358a
IamSubarist <optimistroma927@gmail.com>
2025-10-12 14:29:05 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="bg-gray-200 min-h-[50px] flex items-center px-4">
+      Это будет Footer
+    </div>
+  );
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-gray-200 min-h-[50px] flex items-center px-4">
+      <p>Это будет Header</p>
+    </header>
+  );
+};
+
+export default Header;
